8c97c15e003d422f084df5fd283509723ec6f50f
xjding2 <xjding2@iflytek.com>
2018-01-11 13:44:02 +0800

    demo 试验成功

---
+++ src/components/Page.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        This is page
+        Welcome <span>{{ user }}</span>
+    </div>
+</template>
+<script>
+export default {
+	data () {
+		return {
+			user: 'dingding'
+		}
+	}
+}
+</script>
+<style>
+</style>
